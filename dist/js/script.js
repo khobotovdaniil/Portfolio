870c50b4885b7c59436578bb95908b20fe071af9
khobotovdaniil <dinich44@gmail.com>
2022-05-04 16:28:56 +0300

    added form validation

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,46 @@ const counters = document.querySelectorAll('.skills__raitings-counter'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+$(document).ready(function(){
+
+    //Smooth scroll and pageup
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1200) {
+      $('.pageup').fadeIn();
+    } else $('.pageup').fadeOut();
+  });
+
+  $("a[href^='#up']").click(function() {
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;  
+  });
+
+  $("#form").validate ({
+    rules: {
+      name: {
+        required: true,
+        minlength: 3
+      },  
+      email: {
+        required: true,
+        email: true
+      },
+      checkbox: "required"
+    },
+    messages: {
+      name: {
+        required: "Введите свое имя, пожалуйста",
+        minlength: jQuery.validator.format("Минимальная длина имени {0} символа")
+      },
+      email: {
+        required: "Введите свою почту, пожалуйста",
+        email: "Неправильно введен адрес почты"
+      },
+      checkbox: "Требуется согласие на обработку данных"
+    }
+  });
+
+});
